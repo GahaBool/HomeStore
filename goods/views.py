0123098d3a402af4325ba1a5e00106e2c555522d
Egor Tirskih_laptop <egortirskih1984@gmail.com>
2024-09-04 16:46:33 +0800

    After adding fixtures for goods

--- goods/views.py
+++ goods/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
+
 def catalog(request):
-    return render(request, 'goods/catalog.html')
+    context = {
+        "tiitle": "Home - Каталог",
+        "goods": [
+            {
+                "image": "deps/images/goods/strange table.jpg",
+                "name": "Прикроватный столик",
+                "description": "Столик, довольно странный на вид, но подходит для размещения рядом с кроватью.",
+                "price": 25.00,
+            },
+        ],
+    }
+
+    return render(request, "goods/catalog.html", context)
+
 
 def product(request):
-    return render(request, 'goods/product.html')
+    return render(request, "goods/product.html")
