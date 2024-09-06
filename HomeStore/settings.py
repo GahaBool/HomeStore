b13c3d01991805442f2efa37d9ca00345e838eb9
Egor Tirskih_laptop <egortirskih1984@gmail.com>
2024-09-06 13:15:48 +0800

    Registered simple tag for categories

--- HomeStore/settings.py
+++ HomeStore/settings.py
@@ -129,6 +129,10 @@ INTERNAL_IPS = [
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
     ]
--- goods/views.py
+++ goods/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 
+from goods.models import Categories, Products
+
 
 def catalog(request):
+
+    goods = Products.objects.all()
+    categories = Categories.objects.all()
+
     context = {
         "tiitle": "Home - Каталог",
-        "goods": [
-            {
-                "image": "deps/images/goods/strange table.jpg",
-                "name": "Прикроватный столик",
-                "description": "Столик, довольно странный на вид, но подходит для размещения рядом с кроватью.",
-                "price": 25.00,
-            },
-        ],
+        "goods": goods,
+        "categories": categories,
     }
 
     return render(request, "goods/catalog.html", context)
