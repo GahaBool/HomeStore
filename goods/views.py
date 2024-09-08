46af1858421ba4d4021d3d9ee8fccda19f7e32ae
Egor Tirskih_laptop <egortirskih1984@gmail.com>
2024-09-08 19:21:14 +0800

    Second pagination option - GET parems

--- goods/views.py
+++ goods/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_list_or_404
 from django.core.paginator import Paginator
 
-from goods.models import Categories, Products
+from goods.models import Products
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+
+    page = request.GET.get('page', 1)
     
     if category_slug == 'all':
         goods = Products.objects.all()
@@ -13,7 +15,7 @@ def catalog(request, category_slug, page=1):
 
     
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
 
     context = {
