bbf4c4f924d959e7482dd8a8168e08b952771795
Egor Tirskih_laptop <egortirskih1984@gmail.com>
2024-09-11 16:15:45 +0800

    Profile form, controller & notifications are ready

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
 
 class UserLoginFrom(AuthenticationForm):
 
-        class Meta:
+    class Meta:
         model = User
         firlds = ['username', 'password']
 
@@ -27,3 +27,43 @@ class UserLoginFrom(AuthenticationForm):
     #         'class': 'form-control', 
     #         'placeholder': 'Введите ваш пароль',}),
     # )
+
+class UserRegistrationForm(UserCreationForm):
+    
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+
+class ProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
-from django.contrib import auth
+from email import message
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginFrom
+from users.forms import UserLoginFrom, UserRegistrationForm, ProfileForm
 
 
 def login(request):
@@ -16,6 +18,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, "Вы вошли в аккаунт!")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginFrom()
@@ -27,16 +30,46 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+                form.save()
+                user = form.instance
+                auth.login(request, user)
+                messages.success(request, "Вы успешно зарегистрироавлись!")
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
+
     context = {
-        'title': 'Home - Решистрация'
+        'title': 'Home - Регистрация',
+        'form': form,
     }
+    
     return render(request, 'users/registration.html', context)
 
+@login_required
 def profile(request):
+
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+                form.save()
+                messages.success(request, "Профиль обновлен!")
+                return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+    
     context = {
-        'title': 'Home - Кабинет'
+        'title': 'Home - Кабинет',
+        'form': form,
     }
     return render(request, 'users/profile.html', context)
 
+@login_required
 def logout(request):
-    ...
+    auth.logout(request)
+    messages.success(request, "Вы вышли с аккаунта!")
+    return redirect(reverse("main:index"))
